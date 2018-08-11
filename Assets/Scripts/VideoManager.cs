61fc7acee33b91e9f7055d434d0e2baeb129b9d2
Momin Khan <foolmoron@gmail.com>
2018-08-11 14:28:39 -0400

    Infinite videos + turn off

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Collections.Generic;
+using RenderHeads.Media.AVProVideo;
 
 [Serializable]
 public class VideoLink {
@@ -16,8 +18,74 @@ public class VideoManager : Manager<VideoManager> {
 
     public TextAsset DataJSON;
     public VideoLink[] Links;
+    public List<VideoLink> UsedLinks = new List<VideoLink>(50);
+
+    public GameObject VideoPanelPrefab;
+    GameObject nextVideoPanel;
+
+    public GameObject PanelContainer;
+    public List<VideoPanel> Panels = new List<VideoPanel>(32);
 
     void Awake() {
         Links = JsonUtility.FromJson<VideoLinks>(DataJSON.text).links;
+        var existingPanels = PanelContainer.GetComponentsInChildren<VideoPanel>();
+        existingPanels.ForEach(p => Destroy(p.gameObject));
+        SetupNextVideo();
+    }
+
+    public VideoLink GetUnusedLink() {
+        var link = Links.RandomWhere(UsedLinks, (l, used) => !used.Contains(l));
+        UsedLinks.Add(link);
+        return link;
+    }
+
+    void SetupNextVideo() {
+        nextVideoPanel = Instantiate(VideoPanelPrefab);
+        nextVideoPanel.GetComponent<VideoPanel>().ShouldPlay = true;
+        nextVideoPanel.transform.parent = transform;
+    }
+
+    public void StartNextVideo() {
+        if (nextVideoPanel == null) {
+            SetupNextVideo();
+        }
+
+        var panel = nextVideoPanel;
+        nextVideoPanel = null;
+        SetupNextVideo();
+
+        panel.transform.parent = PanelContainer.transform;
+        panel.transform.SetAsFirstSibling();
+        Panels.Add(panel.GetComponent<VideoPanel>());
+    }
+
+    public void Stop() {
+        Panels.ForEach(p => Destroy(p.gameObject));
+        Panels.Clear();
+        UsedLinks.Clear();
+    }
+
+    void Update() {
+        // gridify vids
+        {
+            var rows = Mathf.FloorToInt(Mathf.Sqrt(Panels.Count) + 0.20f);
+            var columns = Mathf.CeilToInt((float)Panels.Count / rows);
+            if (Screen.height > Screen.width) {
+                var swap = columns;
+                columns = rows;
+                rows = swap;
+            }
+            for (int r = 0; r < rows; r++) {
+                var actualColumns = r == rows - 1 && Panels.Count % columns != 0 ? Panels.Count % columns : columns;
+                for (int c = 0; c < actualColumns; c++) {
+                    var panel = Panels[r * columns + c].GetComponent<RectTransform>();
+                    panel.anchorMin = new Vector2((c + 0) * (1f / actualColumns), (r + 0) * (1f / rows));
+                    panel.anchorMax = new Vector2((c + 1) * (1f / actualColumns), (r + 1) * (1f / rows));
+                    panel.offsetMin = Vector2.zero;
+                    panel.offsetMax = Vector2.zero;
+                    panel.transform.localScale = Vector3.one;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/VideoPanel.cs
+++ Assets/Scripts/VideoPanel.cs
@@ -3,33 +3,68 @@ using System.Collections;
 using System;
 using RenderHeads.Media.AVProVideo;
 
+public enum VideoState { None, Loading, Ready, Waiting, Playing, Error }
 public class VideoPanel : MonoBehaviour {
 
     public VideoLink Link;
-    public MediaPlayerEvent.EventType CurrentState = MediaPlayerEvent.EventType.Closing;
+    public VideoState CurrentState = VideoState.None;
+    public bool ShouldPlay;
+    [Range(0, 10)]
+    public float LoadingTimeout = 6f;
+    float loadingTime;
 
-    MediaPlayer player;
+    public MediaPlayer Player;
 
     void Awake() {
-        player = GetComponent<MediaPlayer>();
-        player.Events.AddListener((mp, e, code) => {
+        Player.Events.AddListener((mp, e, code) => {
             switch (e) {
                 case MediaPlayerEvent.EventType.Closing:
-                    CurrentState = MediaPlayerEvent.EventType.Closing;
+                    CurrentState = VideoState.None;
                     break;
                 case MediaPlayerEvent.EventType.Error:
-                    Debug.LogError("Broken vid: " + Link.tag + " || " + Link.title);
-                    CurrentState = MediaPlayerEvent.EventType.Error;
+                    Debug.LogError("ERROR VID: " + Link.tag + " || " + Link.title);
+                    CurrentState = VideoState.Error;
+                    break;
+                case MediaPlayerEvent.EventType.ReadyToPlay:
+                    CurrentState = VideoState.Ready;
                     break;
                 case MediaPlayerEvent.EventType.Started:
-                    CurrentState = MediaPlayerEvent.EventType.Started;
+                    CurrentState = VideoState.Playing;
                     break;
             }
         });
     }
 
     void Start() {
-        Link = VideoManager.Inst.Links.Random();
-        player.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, Link.vid);
+    }
+
+    void Update() {
+        // loading timeout
+        if (ShouldPlay && CurrentState == VideoState.Loading) {
+            loadingTime += Time.deltaTime;
+            if (loadingTime >= LoadingTimeout) {
+                Debug.LogError("TIMEOUT VID: " + Link.tag + " || " + Link.title);
+                CurrentState = VideoState.Error;
+            }
+        }
+        if (!ShouldPlay || CurrentState != VideoState.Loading) {
+            loadingTime = 0;
+        }
+        // ready to waiting
+        if (ShouldPlay && CurrentState == VideoState.Ready) {
+            CurrentState = VideoState.Waiting;
+        }
+        // load or play
+        switch (CurrentState) {
+            case VideoState.None:
+            case VideoState.Error:
+                Link = VideoManager.Inst.GetUnusedLink();
+                Player.OpenVideoFromFile(MediaPlayer.FileLocation.AbsolutePathOrURL, Link.vid, false);
+                CurrentState = VideoState.Loading;
+                break;
+            case VideoState.Waiting:
+                Player.Play();
+                break;
+        }
     }
 }
