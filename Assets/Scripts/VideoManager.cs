09008a9e3cbd1791e3dfa7c9cd32b814a6348234
Momin Khan <foolmoron@gmail.com>
2018-08-11 01:17:02 -0400

    loading video json

---
+++ Assets/Scripts/VideoManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class VideoLink {
+    public string tag;
+    public string title;
+    public string vid;
+}
+[Serializable]
+public class VideoLinks {
+    public VideoLink[] links;
+}
+public class VideoManager : Manager<VideoManager> {
+
+    public TextAsset DataJSON;
+    public VideoLink[] Links;
+
+    void Awake() {
+        Links = JsonUtility.FromJson<VideoLinks>(DataJSON.text).links;
+    }
+}
